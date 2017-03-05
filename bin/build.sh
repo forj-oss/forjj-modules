b3a6f43be4564ee0bf1655b8e427a0eb2bcaaab3
Christophe Larsonneur <clarsonneur@gmail.com>
2017-03-05 15:39:53 +0100

    Implement build-env to code compilers versions and build process, ready for jenkins.

---
+++ bin/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$BUILD_ENV_LOADED" != "true" ]
+then
+   echo "Please go to your project and load your build environment. 'source build-env.sh'"
+   exit 1
+fi
+
+cd $BUILD_ENV_PROJECT
+
+create-build-env.sh
+
+if [ "$GOPATH" = "" ]
+then
+    echo "Unable to build without GOPATH. Please set it (build)env or your local personal '.be-gopath')"
+    exit 1
+fi
+
+glide i
+
+# Requires forjj to be static.
+export CGO_ENABLED=0
+go test -v forjj-modules/cli
+go test -v forjj-modules/trace
+

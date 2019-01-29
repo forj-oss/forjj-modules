e24902e9b58d33e184aae0ed79a1df70ba208777
Christophe Larsonneur <clarsonneur@gmail.com>
2019-01-29 21:07:52 +0100

    Fix lock file

--- build-env.sh
+++ build-env.sh
@@ -1,6 +1,7 @@
 
 # Build Environment created by buildEnv
 BE_PROJECT=forjj-modules
+export GO_MODULE_REF=github.com/forj-oss/forjj-modules
 
 # Add any module parameters here
 
--- build-unset.sh
+++ build-unset.sh
@@ -2,6 +2,7 @@
 # Build Environment created by buildEnv
 
 # unset any module parameters here
+unset GO_MODULE_REF
 
 unset_build_env
 fcts="`compgen -A function unset`"
